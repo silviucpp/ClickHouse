cbb01b476bc1beb7458050ff36dce29551bf3f72
Silviu Caragea <silviu.caragea@woowteam.com>
2020-05-23 17:17:02 +0300

    Append stats as the last block into the native format

--- src/Processors/Formats/Impl/NativeFormat.cpp
+++ src/Processors/Formats/Impl/NativeFormat.cpp
@@ -4,6 +4,10 @@
 #include <Processors/Formats/IInputFormat.h>
 #include <Processors/Formats/IOutputFormat.h>
 
+#if defined(FL_PATCH)
+#include <Columns/ColumnsNumber.h>
+#include <DataTypes/DataTypesNumber.h>
+#endif
 
 namespace DB
 {
@@ -83,7 +87,11 @@ public:
 
     void onProgress(const Progress & progress) override
     {
+#if defined(FL_PATCH)
+        my_progress.incrementPiecewiseAtomically(progress);
+#else
         stream->onProgress(progress);
+#endif
     }
 
     std::string getContentType() const override
@@ -127,12 +135,19 @@ protected:
     {
         writePrefixIfNot();
         writeSuffixIfNot();
+#if defined(FL_PATCH)
+        writeLastSuffix();
+#endif
     }
 
 private:
     std::shared_ptr<NativeBlockOutputStream> stream;
     bool prefix_written = false;
     bool suffix_written = false;
+#if defined(FL_PATCH)
+    Progress my_progress;
+    Stopwatch watch;
+#endif
 
     void writePrefixIfNot()
     {
@@ -149,6 +164,18 @@ private:
 
         suffix_written = true;
     }
+
+#if defined(FL_PATCH)
+    void writeLastSuffix()
+    {
+        Block block;
+        block.insert(ColumnWithTypeAndName(ColumnUInt8::create(1, 1), std::make_shared<DataTypeUInt8>(), "fl.statistics"));
+        block.insert(ColumnWithTypeAndName(ColumnUInt64::create(1, my_progress.read_rows.load()), std::make_shared<DataTypeUInt64>(), "rows_read"));
+        block.insert(ColumnWithTypeAndName(ColumnUInt64::create(1, my_progress.read_bytes.load()), std::make_shared<DataTypeUInt64>(), "bytes_read"));
+        block.insert(ColumnWithTypeAndName(ColumnFloat64::create(1, watch.elapsedSeconds()), std::make_shared<DataTypeFloat64>(), "elapsed"));
+        stream->write(block);
+    }
+#endif
 };
 
 void registerInputFormatProcessorNative(FormatFactory & factory)
